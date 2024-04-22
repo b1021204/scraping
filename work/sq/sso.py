84c9ba2ba6d08006379117164701c5149a944fa7
nsysk_0101 <b1021204@fun.ac.jp>
2024-04-23 03:45:49 +0900

    ssoのログインまで完了。スクレイピングを次やる

--- work/sq/sso.py
+++ work/sq/sso.py
@@ -4,14 +4,15 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 import pandas as pd 
 import csv
-import pickdle
+import pickle
 
 USER ='b1021204'
 PASS = 'EPa6ouQ2'
 
+
 browser = webdriver.Chrome()
 # login page
-browser.get('https://sso.fun.ac.jp/my.policy')
+browser.get('https://sso.fun.ac.jp')
 
 # sent ID and pass
 '''
@@ -25,28 +26,29 @@ browser.get('https://sso.fun.ac.jp/my.policy')
         
     </tr>
 '''
-elem_username = browser.find_element(By.CSS_SELECTOR, "label_input_1")
-elem_password = browser.find_element(By.CSS_SELECTOR, "label_input_2")
-browser_form = browser.find_element(By.CSS_SELECTOR, "credentials_input_submit")
+elem_username = browser.find_element(By.NAME, "username")
+elem_password = browser.find_element(By.NAME, "password")
+browser_form = browser.find_element(By.CLASS_NAME, "credentials_input_submit")
 #press submit
 elem_username.send_keys(USER)
-elem_username.Send_keys(PASS)
-browser_from.click()
+elem_password.send_keys(PASS)
 
-cookies = driver.get_cookies() # クッキーを取得する
+
+browser_form.click()
+'''
+cookies = driver.get_cookies('https://sso.fun.ac.jp/my.policy') # クッキーを取得する
 pickle.dump(cookies,open(cookies_file,'wb')) # クッキーを保存する
+'''
 print("ログイン成功です!")
 print("\n\n")
-'''
 #取得したい文字列の要素
-elm = browser.find_element(By.CSS_SELECTOR, "")
+elm = browser.find_element(By.CLASS_NAME, "webtop2")
 # 要素のテキスト内容を取得
 num = [elm.text]
-header = ["タイトル"]
+header = [""]
 df = pd.DataFrame({
     '成果':num
 }, index = header)
 df.to_csv("test.csv", encoding="UTF-8")
 
-'''
-#browser.close()
+browser.close()
