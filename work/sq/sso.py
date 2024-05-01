b1145c2bf7480c69c6c4df951311eaca44512b95
nsysk_0101 <b1021204@fun.ac.jp>
2024-05-01 23:32:16 +0900

    ipアドレスを表示するバージョン

--- work/sq/sso.py
+++ work/sq/sso.py
@@ -2,6 +2,7 @@
 
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import pandas as pd 
 import csv
 import pickle
@@ -10,6 +11,13 @@ import time
 USER ='b1021204'
 PASS = 'EPa6ouQ2'
 
+"""
+ブラウザを表示させないようにするコマンド。
+デバックのため、コメントアウト中
+必要ならばwebdriver.Chromeの引数にoptionsを追記
+options = Options()
+options.add_argument('--headless')
+"""
 
 browser = webdriver.Chrome()
 # login page
@@ -33,25 +41,24 @@ browser_form = browser.find_element(By.CLASS_NAME, "credentials_input_submit")
 #press submit
 elem_username.send_keys(USER)
 elem_password.send_keys(PASS)
-
-
 browser_form.click()
 '''
 cookies = driver.get_cookies('https://sso.fun.ac.jp/my.policy') # クッキーを取得する
 pickle.dump(cookies,open(cookies_file,'wb')) # クッキーを保存する
-'''
-print("ログイン成功です!")
-print("\n\n")
-time.sleep(5)
+''' 
+
+# go to page of vm
 elem_vm = browser.find_element(By.ID, "/Common/Manage_P")
 elem_vm.click()
-time.sleep(5)
+
+# go to  page of vm info 
 browser.switch_to.window(browser.window_handles[1])
 elem_select = browser.find_element(By.XPATH, "/html/body/div/div/main/div/form/div[2]/div/span")
 elem_select.click()
-time.sleep(5)
 
-
+elem_ip = browser.find_element(By.ID, "copiable-ip_address-0")
+print("adress : " + elem_ip.text)
+"""
 #取得したい文字列の要素
 elm = browser.find_element(By.CLASS_NAME, "webtop2")
 # 要素のテキスト内容を取得
@@ -63,4 +70,6 @@ df = pd.DataFrame({
 }, index = header)
 df.to_csv("test.csv", encoding="UTF-8")
 
+"""
+print("終了")
 browser.close()
