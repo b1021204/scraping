09d54e0396f8b82c6b8f6869623cc20db87e6781
b1021204 <b1021204@fun.ac.jp>
2024-07-02 16:14:57 +0900

    直接VMページにログイン

--- go_scraping/main.go
+++ go_scraping/main.go
@@ -1,48 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"github.com/sclevine/agouti"
 	"log"
 	"time"
 )
 
-func login(username string, password string) {
-	// ブラウザはChromeを指定して起動
-	driver := agouti.ChromeDriver(agouti.Browser("chrome"))
-	if err := driver.Start(); err != nil {
-		log.Fatalf("Failed to start driver:%v", err)
-	}
-	defer driver.Stop()
-
-	page, err := driver.NewPage()
-	if err != nil {
-		log.Fatalf("Failed to open page:%v", err)
-	} // go to login page
-	if err := page.Navigate("https://sso.fun.ac.jp"); err != nil {
-		log.Fatalf("Failed to navigate:%v", err)
-	}
-
-	// get ID and Password element. Set value
-	elem_user := page.FindByName("username")
-	elem_pass := page.FindByName("password")
-	elem_user.Fill(username)
-	elem_pass.Fill(password)
-	// Submit
-	if err := page.FindByClass("credentials_input_submit").Click(); err != nil {
-		log.Fatalf("Failed to login:%v", err)
-		return
-	} else {
-		fmt.Println("Succece login!!")
-		elem_vm := page.FindByID("/Common/Manage_P")
-		elem_vm.Click()
-		time.Sleep(2 * time.Second)
-
-		return
-	}
-}
-
-func go_to_vmpage(username string, password string) {
+func vmpage(username string, password string) {
 	// ブラウザはChromeを指定して起動
 	driver := agouti.ChromeDriver(agouti.Browser("chrome"))
 	if err := driver.Start(); err != nil {
@@ -56,6 +20,7 @@ func go_to_vmpage(username string, password string) {
 	if err := page.Navigate("https://manage.p.fun.ac.jp/server_manage"); err != nil {
 		log.Fatalf("Failed to navigate:%v", err)
 	}
+	time.Sleep(1 * time.Second)
 
 	elem_user := page.FindByName("username")
 	elem_pass := page.FindByName("password")
@@ -75,6 +40,6 @@ func main() {
 	username := "b1021204"
 	password := "EPa6ouQ2"
 	//login(username, password)
-	go_to_vmpage(username, password)
+	vmpage(username, password)
 
 }
